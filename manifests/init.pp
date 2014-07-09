8daf8c8d64993136241cbcbdf12e35f1f0494e4c
cegeka-jenkins <cegeka-jenkins@cegeka.be>
2014-07-09 10:15:44 +0200

    Initial repository layout for puppet module kmod

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+# Class: kmod
+#
+# This module manages kmod
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class kmod {
+
+
+}
---
+++ spec/classes/kmod_spec.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env rspec
+
+require 'spec_helper'
+
+describe 'kmod' do
+  it { should contain_class 'kmod' }
+end
